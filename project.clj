6a7adf105d0f32ccdfd65bc3cc4126aa1654be2f
Scott <spam.tarpit@gmail.com>
2012-11-21 18:28:17 -0800

    use clojure 1.4.0

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject clj-minecraft "1.0.0-SNAPSHOT"
   :description "Clojure for Bukkit Minecraft"
-  :dependencies [[org.clojure/clojure "1.3.0"]
+  :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.clojure/tools.logging "0.2.3"]
                  [org.clojure/tools.nrepl "0.2.0-beta10"]
                  [org.bukkit/bukkit "1.4.5-R0.3-SNAPSHOT"]]
