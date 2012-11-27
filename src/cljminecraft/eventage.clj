8c5c104d5debb090b2b2741d5418aef3b4553d5e
Scott <spam.tarpit@gmail.com>
2012-11-26 20:53:00 -0800

    update simple event handler to use username

--- src/cljminecraft/eventage.clj
+++ src/cljminecraft/eventage.clj
@@ -2,5 +2,6 @@
     "event handlers for bukkit")
 
 (defn block-break-event [evt]
-      (let [player (.getPlayer evt)]
-        (.sendMessage player "Why you breaka my block?")))
+      (let [player (.getPlayer evt)
+            playerName (.getName player)]
+        (.sendMessage player (.concat "Why you breaka my block, " playerName))))
