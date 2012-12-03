f1bd595ae9eb2277aa7cb84157e67db00768b1a9
basicsensei <basicsensei@gmail.com>
2012-12-03 20:44:19 +0100

    hmm this won't work to not call onDisable when onEnable failed

--- javasrc/cljminecraft/ClojurePlugin.java
+++ javasrc/cljminecraft/ClojurePlugin.java
@@ -4,32 +4,23 @@ import java.lang.ClassLoader;
 
 public class ClojurePlugin extends JavaPlugin {
 	
-    public void loadClojureFile(String cljFile) {
+	public final static String selfCoreScript="cljminecraft.core";
+	public final static String selfEnableFunction="on-enable";
+	public final static String selfDisableFunction="on-disable";
+	public final static String childPlugin_EnableFunction="enable-plugin";
+	public final static String childPlugin_DisableFunction="disable-plugin";
+	
+    private boolean loadClojureFile(String cljFile) {
         try {
             ClassLoader previous = Thread.currentThread().getContextClassLoader();
             Thread.currentThread().setContextClassLoader(this.getClass().getClassLoader());
-
-            System.out.println("loadscript: " + cljFile);
-            clojure.lang.RT.loadResourceScript(cljFile+".clj");
-
-            Thread.currentThread().setContextClassLoader(previous);
-        } catch (Exception e) {
-            System.out.println("Something broke setting up Clojure");
-            e.printStackTrace();
-        }
-    	
-    }
-    
-    public boolean loadClojureNameSpace(String ns) {
-        try {
-            ClassLoader previous = Thread.currentThread().getContextClassLoader();
-            Thread.currentThread().setContextClassLoader(this.getClass().getClassLoader());
-
-            String cljFile = ns.replaceAll("[.]", "/")+".clj";
-            System.out.println("loading clojure file: " + cljFile);
-            clojure.lang.RT.loadResourceScript(cljFile);
-
-            Thread.currentThread().setContextClassLoader(previous);
+			try {
+				System.out.println( "loading clojure file: " + cljFile );
+				clojure.lang.RT.loadResourceScript( cljFile );
+				
+			} finally {
+				Thread.currentThread().setContextClassLoader( previous );
+			}
             return true;
         } catch (Exception e) {
             System.out.println("Something broke setting up Clojure");
@@ -39,65 +30,72 @@ public class ClojurePlugin extends JavaPlugin {
     	
     }
     
+	
+	public final boolean loadClojureNameSpace( String ns ) {
+		String cljFile = ns.replaceAll( "[.]", "/" ) + ".clj";
+		return loadClojureFile( cljFile );
+	}
+    
     public Object invokeClojureFunction(String ns, String funcName) {
     	return clojure.lang.RT.var(ns, funcName).invoke(this);
     }
 
-
     
-    public void onEnableClojureScript(String ns, String enableFunction) {
+    public boolean onEnableClojureChildPlugin(String ns, String enableFunction) {
     	if (loadClojureNameSpace(ns)) {
     		invokeClojureFunction(ns, enableFunction);
-//    		clojure.lang.RT.var(ns, enableFunction).invoke(this);
+    		return true;
+    	}else{
+    		return false;
     	}
-//        try {
-//            ClassLoader previous = Thread.currentThread().getContextClassLoader();
-//            Thread.currentThread().setContextClassLoader(this.getClass().getClassLoader());
-//
-//            System.out.println("loadscript: " + ns.replaceAll("[.]", "/"));
-//            clojure.lang.RT.loadResourceScript(ns.replaceAll("[.]", "/")+".clj");
-//            
-//            clojure.lang.RT.var(ns, enableFunction).invoke(this);
-//            
-//
-//            Thread.currentThread().setContextClassLoader(previous);
-//        } catch (Exception e) {
-//            System.out.println("Something broke setting up Clojure");
-//            e.printStackTrace();
-//        }
     }
 
     @Override
 	public void onEnable() {
-        String name = getDescription().getName();
-        System.out.println("Enabling "+name+" clojure Plugin");
-
-        if ("clj-minecraft".equals(name)) {
-        	onEnableClojureScript("cljminecraft.core", "on-enable");
-        } else {
-        	onEnableClojureScript(name+".core", "enable-plugin");
-        }
+    	boolean errored=false;
+		try {
+			String pluginName = getDescription().getName();
+			System.out.println( "Enabling " + pluginName + " clojure Plugin" );
+			
+			if ( "clj-minecraft".equals( pluginName ) ) {
+				errored=!onEnableClojureChildPlugin( selfCoreScript, selfEnableFunction );
+			} else {
+				errored=!onEnableClojureChildPlugin( pluginName + ".core", childPlugin_EnableFunction );
+			}
+    	}catch(Throwable t) {
+    		errored=true;
+    		t.printStackTrace();
+		} finally {
+			if ( errored ) {
+				setEnabled( !errored );//avoid calling onDisable if onEnable failed! actually this fails because it internally calls onDisable or onEnable if state changed
+			}
+		}
     }
 
-    public void onDisableClojureScript(String ns, String disableFunction) {
-//        clojure.lang.RT.var(ns, disableFunction).invoke(this);
+    public void onDisableClojureChildPlugin(String ns, String disableFunction) {
     	invokeClojureFunction(ns, disableFunction);
     }
 
     @Override
-	public void onDisable() {
-        String name = getDescription().getName();
-        System.out.println("Disabling "+name+" clojure Plugin");
-        if ("clj-minecraft".equals(name)) {
-        	onDisableClojureScript("cljminecraft.core", "on-disable");
+	public void onDisable() {//called only when onEnable didn't fail (if we did the logic right)
+        String pluginName = getDescription().getName();
+        System.out.println("Disabling "+pluginName+" clojure Plugin");
+        if ("clj-minecraft".equals(pluginName)) {
+        	onDisableClojureChildPlugin(selfCoreScript, selfDisableFunction);
         } else {
-            onDisableClojureScript(name+".core", "disable-plugin");
+            onDisableClojureChildPlugin(pluginName+".core", childPlugin_DisableFunction);
         }
     }
     
 /*in plugin.yml of your clojure plugin which depends on clj-minecraft, these are required:
+ * 
  * main: cljminecraft.ClojurePlugin
  * depend: [clj-minecraft]
  * class-loader-of: clj-minecraft
+ * 
+ * and the name of your plugin(in your plugin.yml) should be the ns name of core.clj and core.clj should be the main script 
+ * which includes the enable/disable functions which are defined by the constants: childPlugin_EnableFunction and
+ * childPlugin_DisableFunction in the above
+ * 
   */  
 }
