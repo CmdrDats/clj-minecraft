64cfac59f12a3d399d2731c5a1080683b87f16ac
Deon Moolman <cmdrdats@gmail.com>
2012-12-04 04:46:31 +0200

    Move repl startup into the core/start function, untested, but should work.

--- src/cljminecraft/core.clj
+++ src/cljminecraft/core.clj
@@ -11,14 +11,16 @@
   (log/info "Starting repl on host: %s, port %s" host port)
   (start-server :host host :port port))
 
-(defn on-enable [plugin]
-  (cfg/config-defaults plugin)
+(defn start [plugin]
   (when (cfg/get-boolean plugin "repl.enabled")
     (log/info "Repl options: %s %s %s" (cfg/get-string plugin "repl.host") (cfg/get-int plugin "repl.port") (cfg/get-boolean plugin "repl.enabled"))
     (start-repl (cfg/get-string plugin "repl.host") (cfg/get-int plugin "repl.port"))))
+
+(defn on-enable [plugin]
+  (cfg/config-defaults plugin)
   (when-let [resolved (resolve (symbol (str (.getName plugin) ".core/start")))]
     (resolved plugin))
-  (log/info "Clojure started - %s" plugin))
+  (log/info "Clojure started - %s" plugin)))
 
 (defn on-disable [plugin]
   (log/info "Clojure stopped - %s" plugin))
