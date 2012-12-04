223fa85648acf3316080cf13232097927f01b35e
basicsensei <basicsensei@gmail.com>
2012-12-04 10:08:24 +0100

    cleaning up, this works when *use-context-classloader* is false but not if also current thread's contextclassloader is unset

--- javasrc/cljminecraft/ClojurePlugin.java
+++ javasrc/cljminecraft/ClojurePlugin.java
@@ -73,14 +73,22 @@ public class ClojurePlugin extends BasePlugin {
 	}
 	
 	
+	public void loadClojureResourceScript( String name ) throws IOException {
+		ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+		loadClojureResourceScript(name, classLoader);
+	}
 	/**
 	 * @param name ie. "memorystone/core.clj"
+	 * @param classLoader ie. Thread.currentThread().getContextClassLoader()
 	 * @throws IOException 
 	 */
-	public void loadClojureResourceScript( String name ) throws IOException {
+	public void loadClojureResourceScript( String name, ClassLoader classLoader ) throws IOException {
+		assert null != name;
+		assert name.length() > 0;
+		assert null != classLoader;
+		
 		int slash = name.lastIndexOf( '/' );
 		String file = slash >= 0 ? name.substring( slash + 1 ) : name;//"core.clj"
-		ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
 		InputStream is = classLoader.getResourceAsStream(name);
 		if ( is == null ) {
 			throw new FileNotFoundException( "Can't find Clojure resource `"+name+"` on classpath `" + getClassPath()
@@ -106,74 +114,29 @@ public class ClojurePlugin extends BasePlugin {
 	//but if that's satisfied then config.yml (inside the child's .jar) will be shadowed by cljminecraft(inside its .jar)
 	//due to them using the same classloader (as CmdrDats said)
     private boolean loadClojureFile(String cljFile) {//no synchronized needed
-//    	assert selfPluginName.equals( getDescription().getName() ):"you don't have to call this for other child plugins";
         try {
-        	//note there is a clojure dynamic boolean var, maybe check it: *use-context-classloader*
-        	showClassPath("1",ClassLoader.getSystemClassLoader());
-        	
-			ClassLoader previous = Thread.currentThread().getContextClassLoader();
-			
-			showClassPath("2",previous);
-			showClassPath("3", this.getClass().getClassLoader());
-			
-
-			System.out.println(this.getClass());
 			Class<?> cls =this.getClass(); 
-//					Class.forName("cljminecraft.ClojurePlugin");
-			System.out.println(Class.forName("cljminecraft.ClojurePlugin"));
-
-			showClassPath("6", cls.getClassLoader());
-			System.out.println("classloader of: "+getDescription().getClassLoaderOf());
-//			getDescription().getMain()
-			System.out.println(getDataFolder());
-			System.out.println(this.getClass().getProtectionDomain().getCodeSource().getLocation());
-			System.out.println(Bukkit.getPluginManager().getPlugin(getDescription().getName()).getClass().getProtectionDomain().getCodeSource().getLocation());
-			System.out.println(this.getFile());
-			System.out.println(this.getFile().toURI().toURL());//getAbsoluteFile().toURI());
-//			String path = "/S:/cb/plugins/memorystone-2.0.0-SNAPSHOT.jar";
-//			String urlPath = "jar:file://" + path + "!/";
-//		    addURL (new URL (urlPath));
 			URL urls [] = {this.getFile().toURI().toURL()};
-			URLClassLoader cl = new URLClassLoader(
-//				((URLClassLoader)
-//					this.getClass().getClassLoader()
-//					cls.getClassLoader()
-//					).getURLs()
-					urls
-					,cls.getClassLoader()
-					);
+			URLClassLoader cl = new URLClassLoader( urls, cls.getClassLoader() );
 			
-			
-			Thread.currentThread().setContextClassLoader(
-//				new clojure.lang.DynamicClassLoader(previous)); 
-//				new clojure.lang.DynamicClassLoader(this.getClass().getClassLoader()));
-//				new clojure.lang.DynamicClassLoader(cl));
-				cl);
-//				cls.getClassLoader() );
-//			Thread.currentThread().setContextClassLoader(cl);
+			ClassLoader previous = Thread.currentThread().getContextClassLoader();
+			Thread.currentThread().setContextClassLoader(cl);
 			
 			try {
-				showClassPath("5", Thread.currentThread().getContextClassLoader());
-				showClassPath("4", ClassLoader.getSystemClassLoader());
-
 				System.out.println( "loading clojure file: " + cljFile );
-		        
-				clojure.lang.Var.pushThreadBindings(
-					clojure.lang.RT.map(
-						clojure.lang.RT.USE_CONTEXT_CLASSLOADER
-						, clojure.lang.RT.F));//aka *use-context-classloader* - temp set it to False to see it doesn't affect
-	        	//it's already True by default and it's required to be True else child plugins will fail to load script
+				clojure.lang.Var.pushThreadBindings( clojure.lang.RT.map( clojure.lang.RT.USE_CONTEXT_CLASSLOADER,
+					clojure.lang.RT.F ) );
 				
-				loadClojureResourceScript( cljFile );
-			} finally {
-				try{
+				loadClojureResourceScript( cljFile, cl );
+			}finally {
+//				Thread.currentThread().setContextClassLoader(previous);
+				try {
 					clojure.lang.Var.popThreadBindings();
-				}finally{
+				} finally {
 					Thread.currentThread().setContextClassLoader( previous );
 				}
 			}
-//			System.out.println( "loading clojure file: " + cljFile );
-//			clojure.lang.RT.loadResourceScript( cljFile );
+			
 			return true;
 		} catch ( Exception e ) {
 			System.out.println( "Something broke setting up Clojure" );
