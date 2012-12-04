b20cdd581c2b7272c97b3b73c713eea34a0febd0
basicsensei <basicsensei@gmail.com>
2012-12-04 09:55:29 +0100

    step1 in getting rid of *use-context-classloader* and its default by setting it to False, it works because it's not used

--- javasrc/cljminecraft/ClojurePlugin.java
+++ javasrc/cljminecraft/ClojurePlugin.java
@@ -4,11 +4,10 @@ import org.bukkit.command.*;
 import org.bukkit.plugin.*;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import clojure.lang.*;
-
 import java.io.*;
 import java.lang.ClassLoader;
 import java.net.*;
+import java.nio.charset.*;
 import java.util.*;
 import java.util.logging.*;
 
@@ -24,27 +23,89 @@ public class ClojurePlugin extends BasePlugin {
 	private final static String selfCoreScript="cljminecraft.core";
 	private final static String selfEnableFunction="on-enable";
 	private final static String selfDisableFunction="on-disable";
-	
+	public final static Charset UTF8 = Charset.forName("UTF-8");
 	
 	private void showClassPath(String prefix, ClassLoader cl){
 		System.out.println("=="+prefix+"== For classloader "+cl+" ----------");
-        URL[] urls = ((URLClassLoader)cl).getURLs();
- 
-        for(URL url: urls){
-        	System.out.println(url.getPath());//getFile());
+		System.out.println(getClassPath(cl));
+//        URL[] urls = ((URLClassLoader)cl).getURLs();
+// 
+//        for(URL url: urls){
+//        	System.out.println(url.getPath());//getFile());
+//        	try {
+//				System.out.println(url.toURI());
+//			} catch ( URISyntaxException e ) {
+//				e.printStackTrace();
+//			}
+//        }
+        System.out.println("=="+prefix+"== ----END---"+cl+" ----------");
+	}
+	
+	
+	private final static String getClassPath() {
+		return getClassPath(Thread.currentThread().getContextClassLoader());
+	}
+	
+	private final static String getClassPath(ClassLoader cl) {
+		URL[] urls = ((URLClassLoader)cl).getURLs();
+		String cp ="{";
+		
+		int max = urls.length-1;
+		if (max>=0){
+			cp+=" ";
+		}
+		for ( int i = 0; i <= max; i++ ) {
+			URL url = urls[i];
         	try {
-				System.out.println(url.toURI());
-			} catch ( URISyntaxException e ) {
-				e.printStackTrace();
+				cp+= url.toURI().toString();
+				if(i != max) {
+					cp+=", ";
+				}else {
+					cp+=" ";
+				}
+			} catch ( URISyntaxException use ) {
+				use.printStackTrace();
+				throw new RuntimeException(use);
 			}
         }
-        System.out.println("=="+prefix+"== ----END---"+cl+" ----------");
+        cp+="}";
+        return cp;
+	}
+	
+	
+	/**
+	 * @param name ie. "memorystone/core.clj"
+	 * @throws IOException 
+	 */
+	public void loadClojureResourceScript( String name ) throws IOException {
+		int slash = name.lastIndexOf( '/' );
+		String file = slash >= 0 ? name.substring( slash + 1 ) : name;//"core.clj"
+		ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+		InputStream is = classLoader.getResourceAsStream(name);
+		if ( is == null ) {
+			throw new FileNotFoundException( "Can't find Clojure resource `"+name+"` on classpath `" + getClassPath()
+				+"` using class loader `"+classLoader+"`");
+		}
+		//else
+		
+		try {
+			InputStreamReader isr = new InputStreamReader( is, UTF8 );
+			try {
+				clojure.lang.Compiler.load( isr, name, file );//"memorystone/core.clj" and "core.clj"
+			} finally {
+				isr.close();//FIXME: if this throws it should not overwrite previously thrown exception
+			}
+		} finally {
+			is.close();//FIXME: also this
+		}
+		info( "Loaded Clojure resource `"+name+"` on classpath `" + getClassPath()
+				+"` using class loader `"+classLoader+"`");
 	}
 	
 	//XXX: this works for cljminecraft plugin or for any child plugins having "class-loader-of: cljminecraft" in their plugin.yml
 	//but if that's satisfied then config.yml (inside the child's .jar) will be shadowed by cljminecraft(inside its .jar)
 	//due to them using the same classloader (as CmdrDats said)
-    private synchronized boolean loadClojureFile(String cljFile) {
+    private boolean loadClojureFile(String cljFile) {//no synchronized needed
 //    	assert selfPluginName.equals( getDescription().getName() ):"you don't have to call this for other child plugins";
         try {
         	//note there is a clojure dynamic boolean var, maybe check it: *use-context-classloader*
@@ -97,13 +158,16 @@ public class ClojurePlugin extends BasePlugin {
 
 				System.out.println( "loading clojure file: " + cljFile );
 		        
-	        	Var.pushThreadBindings(RT.map(RT.USE_CONTEXT_CLASSLOADER, RT.T));//aka *use-context-classloader*
+				clojure.lang.Var.pushThreadBindings(
+					clojure.lang.RT.map(
+						clojure.lang.RT.USE_CONTEXT_CLASSLOADER
+						, clojure.lang.RT.F));//aka *use-context-classloader* - temp set it to False to see it doesn't affect
 	        	//it's already True by default and it's required to be True else child plugins will fail to load script
 				
-				clojure.lang.RT.loadResourceScript( cljFile );
+				loadClojureResourceScript( cljFile );
 			} finally {
 				try{
-					Var.popThreadBindings();
+					clojure.lang.Var.popThreadBindings();
 				}finally{
 					Thread.currentThread().setContextClassLoader( previous );
 				}
