3e1ba5628946af8862c019ce97dff395d6b3cb97
basicsensei <basicsensei@gmail.com>
2012-12-05 22:30:31 +0100

    use deploy instead of built.jar symlinked folder in scripts

--- lein_uberjar.bat
+++ lein_uberjar.bat
@@ -1,12 +1,13 @@
 @rem needs this lein.bat https://github.com/technomancy/leiningen/blob/master/bin/lein.bat
-	@rem put it in PATH or near this .bat file
+	@rem put it in PATH
 
 @echo off
 set JAVA_CMD="c:\program files\java\jdk1.6.0_31\bin\java.exe"
 call lein uberjar
 
-rem built.jar is a folder link to your running server's plugin folder
-rem you make it by running this cmd: mklink /d built.jar c:\craftbukkit\plugins\
-if EXIST "built.jar" ( move target\cljminecraft-*standalone*.jar built.jar\cljminecraft.jar )
+set deployDIR=deploy
+rem %deployDIR% is a folder link to your running server's plugin folder
+rem you make it by running this cmd: mklink /d deploy c:\craftbukkit\plugins\
+if EXIST "%deployDIR%" ( move target\cljminecraft-*standalone.jar %deployDIR%\cljminecraft.jar )
 
 if NOT "%1" == "nopause" @pause
