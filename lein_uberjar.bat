d3f3a06f36a36510e23751d71378d01eddafa17e
basicsensei <basicsensei@gmail.com>
2012-12-05 23:05:32 +0100

    script warns when bukkit is running with a fail msg

--- lein_uberjar.bat
+++ lein_uberjar.bat
@@ -8,6 +8,9 @@ call lein uberjar
 set deployDIR=deploy
 rem %deployDIR% is a folder link to your running server's plugin folder
 rem you make it by running this cmd: mklink /d deploy c:\craftbukkit\plugins\
-if EXIST "%deployDIR%" ( move target\cljminecraft-*standalone.jar %deployDIR%\cljminecraft.jar )
+if EXIST "%deployDIR%" ( 
+	move target\cljminecraft-*standalone.jar %deployDIR%\cljminecraft.jar
+	if ERRORLEVEL 1 echo FAILED, make sure bukkit isn't running (so the plugin .jar isn't in locked)
+	)
 
 if NOT "%1" == "nopause" @pause
