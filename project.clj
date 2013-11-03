057516a222ea5ecf95067351e16526cfb99fc57f
Roger Allen <rallen@gmail.com>
2013-11-03 08:29:58 -0800

    Update a few 'easy' dependencies: tools.logging, tools.nrepl and clojure-complete.

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject cljminecraft "1.0.4-SNAPSHOT"
+(defproject cljminecraft "1.0.5-SNAPSHOT"
   :description "Clojure for Bukkit Minecraft"
-  :dependencies [[org.clojure/clojure "1.4.0"]
-                 [org.clojure/tools.logging "0.2.3"]
-                 [org.clojure/tools.nrepl "0.2.0-RC1"]
-                 [org.bukkit/bukkit "1.4.5-R1.0"]
-                 [clojure-complete "0.2.2"]
-                 [cheshire "2.0.4"]
+  :dependencies [[org.clojure/clojure         "1.4.0"]
+                 [org.clojure/tools.logging   "0.2.6"]
+                 [org.clojure/tools.nrepl     "0.2.3"]
+                 [org.bukkit/bukkit           "1.4.5-R1.0"]
+                 [clojure-complete            "0.2.3"]
+                 [cheshire                    "2.0.4"]
                  [org.reflections/reflections "0.9.8"]]
   :profiles {:dev {:dependencies []}}
   :javac-options [ "-d" "classes/" "-source" "1.6" "-target" "1.6"]
